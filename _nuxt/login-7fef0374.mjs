38b3039fe52fcbeff4e5261038bf4b37ec2a51cf
Kai Ho CHAN <Crashes.John@gmail.com>
2022-06-23 14:30:53 +0800

    Initial commit

---
+++ _nuxt/login-7fef0374.mjs
@@ -0,0 +1,1 @@
+import{q as a}from"./entry-9d92ea7f.mjs";var n=a(async e=>{{const t=JSON.parse(localStorage.getItem("walletconnect")||"[]"),o=localStorage.getItem("metamask");if(!(t.connected||o!==null))return console.log("Heading to",e.path,"but you haven't logged in"),"/"}});export{n as default};
